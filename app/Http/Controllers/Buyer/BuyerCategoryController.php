e0bdd442dce9324982d8b5528d99d9833aa9aea4
Ernesto Aides <eaides@hotmail.com>
2019-12-25 21:25:18 +0200

    create soe complex controllers that return relations of relations. Use eagle loading to resolve relations. Implement complex controllers for Buyer and for Transactions. Fix bad functions of relations inside Model Transaction (were protected when must be public) (one controller missed add to git, also add ide-helper generations after composer install)

---
+++ app/Http/Controllers/Buyer/BuyerCategoryController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use App\Http\Controllers\ApiController;
+
+class BuyerCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \App\Buyer  $buyer
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Buyer $buyer)
+    {
+        /**
+         * products<->categories is a relation many to many
+         * therefore the result will be a collection of collections
+         * then MUST use collapse() to unify those collections in one
+         *
+         * products<->seller is a relation one to many
+         * therefore the result will be a unique collection
+         * do not use collapse()
+         */
+        $categories = $buyer->transactions()
+            ->with('product.categories')
+            ->get()
+            ->pluck('product.categories')
+//            ->collapse()
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($categories);
+    }
+
+}
